795fb95dc6df51d016b6147d7664c5bd94911b5d
kazuki-danno <fruit_100@icloud.com>
2019-09-18 15:14:04 +0900

    プロフィール画面を作りました、１回目

--- app/src/main/java/com/example/boichandenwa2/ProfeelActivity.java
+++ app/src/main/java/com/example/boichandenwa2/ProfeelActivity.java
@@ -3,14 +3,12 @@ package com.example.boichandenwa2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class ProfeelActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button returnButton = (Button)findViewById(R.id.button);
+        setContentView(R.layout.activity_profeel);
     }
 }
